396b4fb1b737a5ccbad6994efbca422308ebfe88
nyeinkyawwin02 <nyeinkyawwin11@gmail.com>
2024-11-14 14:10:46 +0630

    Pathway options select update

--- src/components/Home/Pathway copy.vue
+++ src/components/Home/Pathway copy.vue
@@ -7,28 +7,25 @@ import Loading from "../general/Loading.vue";
 import axios from "axios";
 import { useToast } from "vue-toast-notification";
 import { useRouter } from "vue-router";
-
+const countryData = ref();
+const universityList = ref();
+const programList = ref();
+const selectedUniversity = ref();
+const selectedProgram = ref();
 const $toast = useToast();
 const router = useRouter();
+const selectedCountry = ref();
 
 const isLoading = ref(true);
-// option list
-const countryList = ref();
-const programList = ref();
-const courseList = ref();
-const universityList = ref();
 
-// selected data
-const selectedCourse = ref();
-const selectedProgram = ref();
-const selectedCountry = ref(41);
-const selectedUniversity = ref();
+const filters = ref({
+  // country: { name: 'Singapore', slug: 'singapore' },
+});
 
 const currentUniversities = ref([]);
 
 const currentPrograms = ref([]);
 
-// methods
 const updateCurrentCountries = () => {
   currentUniversities.value = universities.filter(
     (university) => university.country === filters.value.country.slug
@@ -53,9 +50,14 @@ const updateSelectedProgram = () => {
   filters.value.program = currentPrograms.value[0];
 };
 
-const handleCountrySelect = (event) => {
-  selectedCountry.value = event.target.value;
-  fetchUniversity();
+const handleCountrySelect = (payload) => {
+  if (payload.index !== "country") {
+    filters.value.country = payload;
+    updateCurrentCountries();
+    updateSelectedUniversity();
+    updateCurrentPrograms();
+    updateSelectedProgram();
+  }
 };
 
 const handleUniversitySelect = (payload) => {
@@ -75,18 +77,15 @@ const handleProgramSelect = (payload) => {
 const fetchCountry = async () => {
   const res = await axios.get("country-lists");
   selectedCountry.value = res.data.countries[0]?.id;
-  countryList.value = res.data.countries;
+  countryData.value = res.data.countries;
 };
 
 // fetch university
 const fetchUniversity = async () => {
-  const res = await axios.get(
-    "university-lists/partner/yes/country/" + selectedCountry.value
-  );
-  // const res = await axios.get("university-lists/partner/yes");
+  const res = await axios.get("university-lists/partner/yes");
   // console.log("uni", res.data.university);
+  selectedUniversity.value = res.data.university[0]?.id;
   universityList.value = res.data.university;
-  selectedUniversity.value = universityList[0]?.id;
 };
 
 // fetch  program
@@ -119,9 +118,8 @@ const searchCountry = async () => {
   }
 };
 const searchHandle = () => {
-  // searchCountry();
-
-  // handle search course
+  searchCountry();
+  // console.log(selectedCountry.value, selectedUniversity.value);
 };
 
 onMounted(() => {
@@ -148,19 +146,18 @@ onMounted(() => {
           education pathway
         </h1>
       </div>
-      <div class="flex sm:flex-col md:flex-row gap-6">
+      <div class="flex ssm:flex-col md:flex-row gap-6">
         <div class="lg:w-[180px]">
           <div class="relative" data-te-dropdown-ref>
             <select
-              v-if="countryList"
+              v-if="countryData"
               name=""
               v-model="selectedCountry"
-              @change="handleCountrySelect"
               class="border min-w-[200px] md:text-md ssm:text-[16px] max-w-[200px] cus-rounded bg-white flex items-center justify-between whitespace-nowrap px-2 pt-2.5 pb-2 font-medium uppercase leading-normal text-cus-primary transition duration-75 ease-in-out focus:outline-none focus:ring-0 motion-reduce:transition-none"
             >
               <option
                 class="text-[13px]"
-                v-for="data in countryList"
+                v-for="data in countryData"
                 :key="data.id"
                 :value="data.id"
               >
@@ -210,6 +207,66 @@ onMounted(() => {
           </div>
         </div>
         <div class="w-[200px]">
+          <div class="relative" data-te-dropdown-ref>
+            <select
+              v-if="universityList"
+              name=""
+              v-model="selectedUniversity"
+              class="border md:text-md ssm:text-[16px] max-w-[200px] cus-rounded bg-white flex items-center justify-between whitespace-nowrap px-2 pt-2.5 pb-2 font-medium uppercase leading-normal text-cus-primary transition duration-75 ease-in-out focus:outline-none focus:ring-0 motion-reduce:transition-none"
+            >
+              <option
+                class="overflow-x-scroll text-[13px]"
+                v-for="data in universityList"
+                :key="data.id"
+                :value="data.id"
+              >
+                {{ data.university_name }}
+              </option>
+            </select>
+            <!-- <button
+              class="w-full border cus-rounded bg-white flex items-center justify-between overflow-auto whitespace-nowrap px-6 pt-2.5 pb-2 md:text-md ssm:text-[16px] font-medium uppercase leading-normal text-cus-primary transition duration-75 ease-in-out focus:outline-none focus:ring-0 motion-reduce:transition-none"
+              href="#"
+              type="button"
+              id="dropdownMenuButton3"
+              data-te-dropdown-toggle-ref
+              aria-expanded="false"
+              data-te-ripple-init
+              data-te-ripple-color="light"
+            >
+              {{ filters?.uni?.name ?? "University" }}
+              <span class="ml-2 w-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  class="h-5 w-5"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </span>
+            </button> -->
+            <!-- <ul
+              class="max-h-[300px] w-full absolute cus-rounded z-[1000] float-left m-0 hidden list-none overflow-y-scroll overflow-x-auto border-none bg-white bg-clip-padding text-left text-base shadow-lg dark:bg-neutral-700 [&[data-te-dropdown-show]]:block duration-75"
+              aria-labelledby="dropdownMenuButton3"
+              data-te-dropdown-menu-ref
+            >
+              <li v-for="(uni, index) in currentUniversities" :key="index">
+                <div
+                  @click="handleUniversitySelect(uni)"
+                  class="block w-full whitespace-nowrap bg-transparent py-2 px-4 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600"
+                  data-te-dropdown-item-ref
+                >
+                  {{ uni.name }}
+                </div>
+              </li>
+            </ul> -->
+          </div>
+        </div>
+        <div class="lg:w-[170px] md:w-[200px] md:-ml-5">
           <div class="relative" data-te-dropdown-ref>
             <select
               v-if="programList"
@@ -267,25 +324,6 @@ onMounted(() => {
                 </div>
               </li>
             </ul> -->
-          </div>
-        </div>
-        <div class="lg:w-[170px] md:w-[200px] md:-ml-5">
-          <div class="relative" data-te-dropdown-ref>
-            <select
-              v-if="courseList"
-              name=""
-              v-model="selectedCourse"
-              class="border md:text-md ssm:text-[16px] max-w-[200px] cus-rounded bg-white flex items-center justify-between whitespace-nowrap px-2 pt-2.5 pb-2 font-medium uppercase leading-normal text-cus-primary transition duration-75 ease-in-out focus:outline-none focus:ring-0 motion-reduce:transition-none"
-            >
-              <option
-                class="overflow-x-scroll text-[13px]"
-                v-for="data in courseList"
-                :key="data.id"
-                :value="data.id"
-              >
-                {{ data.university_name }}
-              </option>
-            </select>
           </div>
         </div>
 
