290595fa3f1c5bd1a8efac72a94505ab235b24b8
nyeinkyawwin02 <nyeinkyawwin11@gmail.com>
2024-11-16 10:28:10 +0630

    Display University List by Searching

--- src/components/Home/Pathway.vue
+++ src/components/Home/Pathway.vue
@@ -12,6 +12,7 @@ const $toast = useToast();
 const router = useRouter();
 
 const isLoading = ref(true);
+const uniLoading = ref(true);
 // option list
 const countryList = ref();
 const programList = ref();
@@ -61,6 +62,7 @@ const getCourses = async () => {
     const formattedArray = array.map((course) => course.trim());
     courseList.value = formattedArray;
 
+    selectedCourse.value = "default";
     return;
   }
   selectedCourse.value = "default";
@@ -76,6 +78,10 @@ const handleProgramSelect = (event) => {
   selectedProgramId.value = event.target.value;
   selectedProgram.value = programList.value.filter(program => program.id == selectedProgramId.value)?.[0];
 };
+
+const handleCourseSelect = (event) => {
+  selectedCourse.value = event.target.value;
+}
 
 const checkCountry = () => {
   if (selectedCountry.value) {
@@ -97,32 +103,61 @@ const checkProgramId = () => {
   return false;
 }
 
-const handleSearch = async () => {
-  let universityArray = [];
-
-  if (selectedCountry.value) {
-    if (selectedCountry.value !== "default") {
-      const res = await axios.get(
-        "university-lists/country/" + selectedCountry.value
-      );
-      universityArray = res.data.university;
-
-      if (checkProgramId() && universityArray.length > 0) {
-        const universityIds = [...new Set(programList.value.map(program => program.pivot.university_id))];
-
-        universityArray = universityArray.filter(university => universityIds.includes(university.id));
-      }
-
-      filteredUniversityList.value = universityArray;
-
-      return;
+const checkCourse = () => {
+  if (selectedCourse.value) {
+    if (selectedCourse.value !== "default") {
+      return true;
     }
   }
-  $toast.error('Not Found !', { position: 'top-right' });
+
+  return false;
 }
 
-// done to this point
+const handleSearch = async () => {
+  try {
+    let universityArray = [];
+    uniLoading.value = true;
 
+    if (selectedCountry.value) {
+      if (selectedCountry.value !== "default") {
+        const res = await axios.get(
+          "university-lists/country/" + selectedCountry.value
+        );
+        universityArray = res.data.university;
+
+        if (universityArray.length <= 0) {
+          $toast.error('Not Found !', { position: 'top-right' });
+          filteredUniversityList.value = universityArray;
+
+          return;
+        }
+
+        if (checkCourse()) {
+          const filterProgramArray = programList.value.filter(program => program.pivot.course_name.includes(selectedCourse.value));
+
+          universityArray = handleUniversityFilteredByPrograms(filterProgramArray, universityArray);
+        } else {
+          if (checkProgramId()) {
+            universityArray = handleUniversityFilteredByPrograms(programList.value, universityArray);
+          }
+        }
+
+        filteredUniversityList.value = universityArray;
+
+        return;
+      }
+    }
+    $toast.error('Not Found !', { position: 'top-right' });
+  } finally {
+    uniLoading.value = false;
+  }
+}
+
+const handleUniversityFilteredByPrograms = (programArray, universityArray) => {
+  const universityIds = [...new Set(programArray.map(program => program.pivot.university_id))];
+
+  return universityArray.filter(university => universityIds.includes(university.id));
+}
 
 const updateCurrentCountries = () => {
   currentUniversities.value = universities.filter(
@@ -199,6 +234,7 @@ onMounted(() => {
   isLoading.value = false;
   fetchCountries();
   fetchPrograms();
+  handleSearch();
   // fetchUniversities();
 });
 
@@ -215,109 +251,131 @@ watch(
     <Loading />
   </template>
   <template v-else>
-    <div
-      class="flex md:flex-col ssm:mt-3 md:mt-0 ssm:flex-col md:space-y-5 lg:space-y-0 lg:flex-row justify-between items-center"
-    >
-      <div class="flex flex-col select-none">
-        <h1 class="cus-subheading text-cus-primary uppercase">
-          find your right
-        </h1>
-        <h1 class="cus-subheading text-cus-secondary uppercase">
-          education pathway
-        </h1>
-      </div>
-      <div class="flex sm:flex-col md:flex-row gap-6">
-        <div class="lg:w-[180px]">
-          <div class="relative" data-te-dropdown-ref>
-            <select
-              name=""
-              v-model="selectedCountry"
-              @change="handleCountrySelect"
-              class="border min-w-[200px] md:text-md ssm:text-[16px] max-w-[200px] cus-rounded bg-white flex items-center justify-between whitespace-nowrap px-2 pt-2.5 pb-2 font-medium uppercase leading-normal text-cus-primary transition duration-75 ease-in-out focus:outline-none focus:ring-0 motion-reduce:transition-none"
-            >
-              <option
-                class="overflow-x-scroll text-[13px]"
-                value="default"
-              >
-                - Country -
-              </option>
-              <template 
-                v-if="countryList"
+    <div class="flex flex-col gap-12">
+      <div
+        class="flex md:flex-col ssm:mt-3 md:mt-0 ssm:flex-col md:space-y-5 lg:space-y-0 lg:flex-row justify-between items-center"
+      >
+        <div class="flex flex-col select-none">
+          <h1 class="cus-subheading text-cus-primary uppercase">
+            find your right
+          </h1>
+          <h1 class="cus-subheading text-cus-secondary uppercase">
+            education pathway
+          </h1>
+        </div>
+        <div class="flex sm:flex-col md:flex-row gap-6">
+          <div class="lg:w-[180px]">
+            <div class="relative" data-te-dropdown-ref>
+              <select
+                name=""
+                v-model="selectedCountry"
+                @change="handleCountrySelect"
+                class="border min-w-[200px] md:text-md ssm:text-[16px] max-w-[200px] cus-rounded bg-white flex items-center justify-between whitespace-nowrap px-2 pt-2.5 pb-2 font-medium uppercase leading-normal text-cus-primary transition duration-75 ease-in-out focus:outline-none focus:ring-0 motion-reduce:transition-none"
               >
                 <option
+                  class="overflow-x-scroll text-[13px]"
+                  value="default"
+                >
+                  - Country -
+                </option>
+                <template 
+                  v-if="countryList"
+                >
+                  <option
+                    class="text-[13px]"
+                    v-for="data in countryList"
+                    :key="data.id"
+                    :value="data.id"
+                  >
+                    {{ data.name }}
+                  </option>
+                </template>
+              </select>
+            </div>
+          </div>
+          <div class="w-[200px]">
+            <div class="relative" data-te-dropdown-ref>
+              <select
+                v-if="programList"
+                v-model="selectedProgramId"
+                name=""
+                @change="handleProgramSelect"
+                class="border md:text-md ssm:text-[16px] max-w-[200px] cus-rounded bg-white flex items-center justify-between whitespace-nowrap px-2 pt-2.5 pb-2 font-medium uppercase leading-normal text-cus-primary transition duration-75 ease-in-out focus:outline-none focus:ring-0 motion-reduce:transition-none"
+              >
+                <option
+                  class="overflow-x-scroll text-[13px]"
+                  value="default"
+                >
+                  - Program -
+                </option>
+                <option
                   class="text-[13px]"
-                  v-for="data in countryList"
+                  v-for="data in programList"
                   :key="data.id"
                   :value="data.id"
                 >
                   {{ data.name }}
                 </option>
-              </template>
-            </select>
+              </select>
+            </div>
           </div>
-        </div>
-        <div class="w-[200px]">
-          <div class="relative" data-te-dropdown-ref>
-            <select
-              v-if="programList"
-              v-model="selectedProgramId"
-              name=""
-              @change="handleProgramSelect"
-              class="border md:text-md ssm:text-[16px] max-w-[200px] cus-rounded bg-white flex items-center justify-between whitespace-nowrap px-2 pt-2.5 pb-2 font-medium uppercase leading-normal text-cus-primary transition duration-75 ease-in-out focus:outline-none focus:ring-0 motion-reduce:transition-none"
-            >
-              <option
-                class="overflow-x-scroll text-[13px]"
-                value="default"
-              >
-                - Program -
-              </option>
-              <option
-                class="text-[13px]"
-                v-for="data in programList"
-                :key="data.id"
-                :value="data.id"
-              >
-                {{ data.name }}
-              </option>
-            </select>
-          </div>
-        </div>
-        <div class="lg:w-[170px] md:w-[200px] md:-ml-5">
-          <div class="relative" data-te-dropdown-ref>
-            <select
-              name=""
-              v-model="selectedCourse"
-              class="border md:text-md ssm:text-[16px] max-w-[200px] cus-rounded bg-white flex items-center justify-between whitespace-nowrap px-2 pt-2.5 pb-2 font-medium uppercase leading-normal text-cus-primary transition duration-75 ease-in-out focus:outline-none focus:ring-0 motion-reduce:transition-none"
-            >
-              <option
-                class="overflow-x-scroll text-[13px]"
-                value="default"
-              >
-                - Course -
-              </option>
-              <template 
-                v-if="courseList"
+          <div class="lg:w-[170px] md:w-[200px] md:-ml-5">
+            <div class="relative" data-te-dropdown-ref>
+              <select
+                name=""
+                v-model="selectedCourse"
+                @change="handleCourseSelect"
+                class="border md:text-md ssm:text-[16px] max-w-[200px] cus-rounded bg-white flex items-center justify-between whitespace-nowrap px-2 pt-2.5 pb-2 font-medium uppercase leading-normal text-cus-primary transition duration-75 ease-in-out focus:outline-none focus:ring-0 motion-reduce:transition-none"
               >
                 <option
                   class="overflow-x-scroll text-[13px]"
-                  v-for="(course, idx) in courseList"
-                  :key="idx"
-                  :value="idx"
+                  value="default"
                 >
-                  {{ course }}
+                  - Course -
                 </option>
-              </template>
-            </select>
+                <template 
+                  v-if="courseList"
+                >
+                  <option
+                    class="overflow-x-scroll text-[13px]"
+                    v-for="(course, idx) in courseList"
+                    :key="idx"
+                    :value="course"
+                  >
+                    {{ course }}
+                  </option>
+                </template>
+              </select>
+            </div>
           </div>
-        </div>
 
-        <Button
-          @click="handleSearch"
-          class="flex justify-center md:ml-4 md:px-6 items-center"
-          type="gradient"
-          >Search</Button
-        >
+          <Button
+            @click="handleSearch"
+            class="flex justify-center md:ml-4 md:px-6 items-center"
+            type="gradient"
+            >Search</Button
+          >
+        </div>
       </div>
+
+      <template v-if="uniLoading">
+        <Loading />
+      </template>
+      <template v-else>
+        <div v-if="filteredUniversityList.length > 0" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 first-letter justify-items-center gap-12">
+          <template v-for="university in filteredUniversityList" :key="university">
+            <a :href="university.university_link" target="_blank" class="block  bg-white w-full h-[150px] group relative cus-standout overflow-hidden">
+              <img class="w-full h-full object-contain p-3" :src="university.imageURL" />
+              <div class="flex items-center justify-center absolute top-0 left-0 group-hover:bg-gray-900 group-hover:bg-opacity-40 w-full h-full transition">
+                <span class="opacity-0 group-hover:opacity-100 transition z-10 text-white text-center">{{ university.university_name }}</span>
+              </div>
+            </a>
+          </template>
+        </div>
+        <div v-else class="flex md:flex-col ssm:mt-3 md:mt-0 ssm:flex-col md:space-y-5 justify-between items-center">
+          No University Found!
+        </div>
+      </template>
     </div>
   </template>
 </template>
